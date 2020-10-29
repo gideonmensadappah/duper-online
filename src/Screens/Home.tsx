4b9f18de108f373a6be8d78c3ba5c5974c04c32b
Gideon Mensa <gideon123ewq@gmail.com>
2020-10-29 23:08:07 +0200

    products list to FlatList component

--- src/Screens/Home.tsx
+++ src/Screens/Home.tsx
@@ -1,16 +1,33 @@
 import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View, FlatList } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import Card from "../Components/Card";
+
 type Props = {
   navigation: StackNavigationProp<any, "About">;
 };
 
+const data = Array.from({ length: 4 }, (_, index) => ({
+  id: `${index}`,
+  title: `List ${index}`,
+  subtitle: "20 Items",
+}));
 const HomeScreen: React.FC<Props> = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Card title="Family Shopping List" subtitle="World!"></Card>
+      <FlatList
+        style={styles.list}
+        keyExtractor={({ id }) => id}
+        data={data}
+        renderItem={({ item: { id, title, subtitle } }) => (
+          <Card
+            title={title}
+            listId={id}
+            navigation={navigation}
+            subtitle={subtitle}
+          />
+        )}
+      />
     </View>
   );
 };
@@ -18,9 +35,14 @@ const HomeScreen: React.FC<Props> = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    backgroundColor: "#F1F1F1",
+    alignItems: "stretch",
     justifyContent: "center",
+    paddingHorizontal: "8px",
+    paddingVertical: "16px",
+  },
+  list: {
+    flex: 1,
   },
 });
 
